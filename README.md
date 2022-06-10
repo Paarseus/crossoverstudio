e2a0f446385e3330fc59289eb87bd841b005d3e3
Paarseus <100014381+Paarseus@users.noreply.github.com>
2022-06-10 14:05:31 +0430

    Update

--- README.md
+++
@@ -1,5 +0,0 @@
-# crossoverstudio.ir
-This is the first website i have designed so i published it here if you find issues(i know theres a lot) or any suggestions ill be happy for letting me to know.
-
-  My Instagram and Telegram: @ParsaGhs
-  
---
+++ vid/texts/descriptions.js
@@ -0,0 +1,21 @@
+const spotify_links = [];
+spotify_links[0] = "https://open.spotify.com/embed/track/268mP4si3ew7OB5uTtZ2NI?utm_source=generator&theme=0";
+spotify_links[1] = "https://open.spotify.com/embed/track/6Zt6pIKGyFYbGSQJmZgXdC?utm_source=generator&theme=0";
+spotify_links[2] = "https://open.spotify.com/embed/track/6ToO9noN2nDt4nI94p2lFa?utm_source=generator&theme=0";
+spotify_links[3] = "https://open.spotify.com/embed/track/0MSURNjdjwttLHnVpBop95?utm_source=generator"
+
+const des = [];
+des[0] = "none";
+des[1] = "Toulani Free Style: @tiktaaksaesina <br>Music: @raadfar_  <br>Video: @rezavaezi";
+des[2] = "Breaking Battle Colony <br>Sarkoob: @yastunes <br>Music: @raadfar_ ";
+des[3] = "Video: @hossein_dehghani33 <br>";
+des[4] = "Bboys: @bboyfarzin @cna_western <br>@saeed.sharifinejad @sajad.bp <br>@bboy.morteza @bboylock"
+des[5] = "none";
+
+const tname = [];
+tname[0] = 'none';
+tname[1] = 'bboygrubby';
+tname[2] = 'bboygrubby';
+tname[3] = 'bboygrubby';
+tname[4] = 'capitalcrew';
+
